18a8f6da6361ac267b2a87aeedb4c38b6d376b92
“a_villarejo19” <a.villarejo19@gmail.com>
2019-01-27 23:28:14 -0600

    first commit

---
+++ data/db.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/clientes', { useNewUrlParser: true });
+
+//Definir schema de Clientes
+const clientesSchema = new mongoose.Schema({
+	nombre: String,
+	apellido: String,
+	empresa: String,
+	email: String,
+	edad: Number,
+	tipo: String,
+	pedidos: Array
+});
+
+//Modelo de Clientes
+const Clientes = mongoose.model('clientes', clientesSchema);
+
+export { Clientes };
---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './data/schema';
+import { resolvers } from './data/resolvers';
+
+//Creaccion de la aplicacion de express
+const app = express();
+
+// const cors = require('cors');
+// app.use(cors());
+
+//Creaccion del servidor de apollo, requiere defs y resolvers
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//Coneccion entre Apollo y express
+server.applyMiddleware({ app });
+
+//Ejecucion del Servidor
+const port = 9000;
+app.listen({ port }, () => console.log(`El servidor esta corriendo - http://localhost:${port}${server.graphqlPath}`));
